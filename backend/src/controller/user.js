28b75355c3fc23c983e186a55f91f4f8ce47b50c
Sourav Kumar <kmrsourav786@gmail.com>
2023-04-08 23:09:59 +0530

    user signup route completed

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -22,6 +22,7 @@ exports.signup = (req, res) => {
             }
         })
     } catch (err) {
+        console.log(err)
         return res.status(400).json({
             message: "Something went wrong"
         })
